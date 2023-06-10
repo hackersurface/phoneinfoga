4c331bb93c7638d3d628d3350a94aabdb4f3da41
hackersurface <135252157+hackersurface@users.noreply.github.com>
2023-06-10 12:54:19 +0100

    Update root.go
    Hacker/phoneinfoga

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ var rootCmd = &cobra.Command{
 	Use:     "phoneinfoga [COMMANDS] [OPTIONS]",
 	Short:   "Advanced information gathering & OSINT tool for phone numbers",
 	Long:    "PhoneInfoga is one of the most advanced tools to scan phone numbers using only free resources.",
-	Example: "phoneinfoga scan -n <number>",
+	Example: "phoneinfoga scan -n <447484196310>",
 }
 
 // Execute is a function that executes the root command
